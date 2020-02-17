2d5c8c422370e3240554dee1a6d0f18c6c1a771e
valeria campilongo <cvaleriac@hotmail.it>
2020-02-17 21:15:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,17 +12,3 @@ function changeLeastFavoriteCustomer(){
 function setBestCustomer(){
   bestCustomer = 'not bob'
 }
-
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
-}
-
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
-
-}
