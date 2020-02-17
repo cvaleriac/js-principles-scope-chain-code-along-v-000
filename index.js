e907efee0cbcd7d9e6842709fadf3e77010043ec
valeria campilongo <cvaleriac@hotmail.it>
2020-02-17 21:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,25 @@ var customerName = 'bob'
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
 }
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'sour fred'
+}
+
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(newFavorite){
+  bestCustomer = newFavorite
+}
+
+function congratulateCustomer(){
+  return `congrats ${favoriteCustomer}`
+}
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = 'bar'
+    let favoriteCustomer = 'not bar'
+    
+}
